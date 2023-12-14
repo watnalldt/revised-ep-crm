ca1f0b88107b88538a97da668840e37d1530434c
watnalldt <davidaturner56@yahoo.com>
2023-12-14 06:19:30 +0000

    Register utilities and clients in admin

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from clients.models import Client
+
+
 # Register your models here.
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    pass
--- utilities/admin.py
+++ utilities/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from utilities.models import Supplier, Utility
+
+
 # Register your models here.
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Utility)
+class UtilityAdmin(admin.ModelAdmin):
+    pass
